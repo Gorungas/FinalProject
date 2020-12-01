655e7b7c878bad9f8b10f4031517c8f4000cca31
TheBruceSwain <jackson9w9@gmail.com>
2020-12-01 00:10:42 -0800

    V1.0 Of Prep Cook Simulator. You can now chop tomatoes with your dextrose knife skills. Hoorah! Built kitchen environment, models, imported FPS Controller.

---
+++ Assets/PickUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PickUp : MonoBehaviour
+{
+    public Transform dest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = this.dest.position;
+        this.transform.parent = GameObject.Find("Destination").transform;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll | RigidbodyConstraints.FreezeRotation;
+
+    }
+
+    private void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+    }
+}
---
+++ Assets/Slice.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slice : MonoBehaviour
+{
+    public GameObject tomato;
+    public GameObject tomatochild1;
+    public GameObject tomatochild2;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "knife")
+        {
+            tomatochild1.transform.parent = null;
+            tomatochild2.transform.parent = null;
+            tomato.SetActive(false);
+            tomatochild1.SetActive(true);
+            tomatochild2.SetActive(true);
+        }
+    }
+}
